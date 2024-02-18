575afff33bec20c6f9c5f3811c2e0f75a18af8df
wasdfer123 <mburli3381@mac-book.attlocal.net>
2024-02-18 16:58:15 -0700

    netheriamond sword added

---
+++ src/main/java/net/mcreator/kersdfersmod/init/KersdfersmodModItems.java
@@ -0,0 +1,19 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.kersdfersmod.init;
+
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.DeferredRegister;
+
+import net.minecraft.world.item.Item;
+
+import net.mcreator.kersdfersmod.item.NetheriamondSwordItem;
+import net.mcreator.kersdfersmod.KersdfersmodMod;
+
+public class KersdfersmodModItems {
+	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, KersdfersmodMod.MODID);
+	public static final RegistryObject<Item> NETHERIAMOND_SWORD = REGISTRY.register("netheriamond_sword", () -> new NetheriamondSwordItem());
+}
---
+++ src/main/java/net/mcreator/kersdfersmod/init/KersdfersmodModTabs.java
@@ -0,0 +1,29 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.kersdfersmod.init;
+
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.core.registries.Registries;
+
+import net.mcreator.kersdfersmod.KersdfersmodMod;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class KersdfersmodModTabs {
+	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, KersdfersmodMod.MODID);
+
+	@SubscribeEvent
+	public static void buildTabContentsVanilla(BuildCreativeModeTabContentsEvent tabData) {
+
+		if (tabData.getTabKey() == CreativeModeTabs.COMBAT) {
+			tabData.accept(KersdfersmodModItems.NETHERIAMOND_SWORD.get());
+		}
+	}
+}
