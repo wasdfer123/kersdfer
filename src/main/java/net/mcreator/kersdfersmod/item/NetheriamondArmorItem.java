7c28476499a8b2ed32a5c5f6e7d2109a77bd7051
wasdfer123 <mburli3381@mac-book.attlocal.net>
2024-02-18 18:14:57 -0700

    hilts updated

--- src/main/java/net/mcreator/kersdfersmod/item/NetheriamondArmorItem.java
+++ src/main/java/net/mcreator/kersdfersmod/item/NetheriamondArmorItem.java
@@ -1,12 +1,23 @@
 
 package net.mcreator.kersdfersmod.item;
 
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundEvent;
-import java.util.function.Consumer;
-import net.minecraft.client.model.Model;
+import net.minecraft.network.chat.Component;
+
+import java.util.List;
 
 public abstract class NetheriamondArmorItem extends ArmorItem {
-
 	public NetheriamondArmorItem(ArmorItem.Type type, Item.Properties properties) {
 		super(new ArmorMaterial() {
 			@Override
@@ -52,7 +63,6 @@ public abstract class NetheriamondArmorItem extends ArmorItem {
 	}
 
 	public static class Helmet extends NetheriamondArmorItem {
-
 		public Helmet() {
 			super(ArmorItem.Type.HELMET, new Item.Properties());
 		}
@@ -71,11 +81,9 @@ public abstract class NetheriamondArmorItem extends ArmorItem {
 		public boolean makesPiglinsNeutral(ItemStack itemstack, LivingEntity entity) {
 			return false;
 		}
-
 	}
 
 	public static class Chestplate extends NetheriamondArmorItem {
-
 		public Chestplate() {
 			super(ArmorItem.Type.CHESTPLATE, new Item.Properties());
 		}
@@ -94,11 +102,9 @@ public abstract class NetheriamondArmorItem extends ArmorItem {
 		public boolean makesPiglinsNeutral(ItemStack itemstack, LivingEntity entity) {
 			return false;
 		}
-
 	}
 
 	public static class Leggings extends NetheriamondArmorItem {
-
 		public Leggings() {
 			super(ArmorItem.Type.LEGGINGS, new Item.Properties());
 		}
@@ -117,11 +123,9 @@ public abstract class NetheriamondArmorItem extends ArmorItem {
 		public boolean makesPiglinsNeutral(ItemStack itemstack, LivingEntity entity) {
 			return false;
 		}
-
 	}
 
 	public static class Boots extends NetheriamondArmorItem {
-
 		public Boots() {
 			super(ArmorItem.Type.BOOTS, new Item.Properties());
 		}
@@ -140,7 +144,5 @@ public abstract class NetheriamondArmorItem extends ArmorItem {
 		public boolean makesPiglinsNeutral(ItemStack itemstack, LivingEntity entity) {
 			return false;
 		}
-
 	}
-
 }
--- src/main/java/net/mcreator/kersdfersmod/item/NetheriamondShovelItem.java
+++ src/main/java/net/mcreator/kersdfersmod/item/NetheriamondShovelItem.java
@@ -1,7 +1,21 @@
 
 package net.mcreator.kersdfersmod.item;
 
-import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.ShovelItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+
+import net.mcreator.kersdfersmod.init.KersdfersmodModItems;
+
+import java.util.List;
 
 public class NetheriamondShovelItem extends ShovelItem {
 	public NetheriamondShovelItem() {
